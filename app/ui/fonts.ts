06c84bacd62027967ad0e872dc2bb62485929c65
MKSBRYAN <officielsamuelmok63@>
2024-12-25 05:34:56 -0800

    First commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+
+import {JetBrains_Mono,Lusitana} from 'next/font/google'
+
+export const inter=JetBrains_Mono({subsets:['latin']});
+
+export const lusitana=Lusitana({weight:['400','700'],subsets:['latin']})
